f4cd0dd4bc2f4a226b433baee2bb1f4ee3f877b0
Sandhya Rani Simhadri <sandhyasimhadri999@gmail.com>
2025-02-25 11:46:06 +0530

    update apis

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
   hmr: false,
-  apiUrl: 'http://51.20.122.168/',
+  apiUrl: 'https://51.20.122.168/',
   apiUrl_web: "http://localhost:8000/",
   // apiUrl_fe:"http://localhost:4200/",
-  apiUrl_fe:"http://16.16.79.221/",
+  apiUrl_fe:"https://16.16.79.221/",
   recaptcha: {
     siteKey: '6LfhKxUqAAAAADTIEBB4IRPhETpFycTuWF0Svb-6',
   },
